5c359a983d00108cd3a1e2ba03d83367562eabdd
snaptower123 <snaptower123@gmail.com>
2026-06-06 23:29:07 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,15 +1,13 @@
 const CACHE_NAME = 'snaptower-v1';
-const urlsToCache = [
+const ASSETS = [
   '/',
   '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/manifest.json'
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
   self.skipWaiting();
 });
@@ -25,6 +23,6 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(cached => cached || fetch(event.request))
   );
 });
